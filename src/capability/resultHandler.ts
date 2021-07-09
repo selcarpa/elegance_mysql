6765c700625aeee1ceb8cff57664bfb1ef20546e
AethLi <selcarpa@gmail.com>
2021-07-09 17:50:05 +0800

    coding

--- src/capability/resultHandler.ts
+++ src/capability/resultHandler.ts
@@ -1,11 +1,48 @@
+import { FieldPacket, OkPacket, ResultSetHeader } from "mysql2";
+import { WebviewPanel } from "vscode";
+import { Message, QueryMessage } from "../model/messageModel";
+import { Logger } from "./logService";
+import { openQueryHtml } from "./viewsUtils";
+
 export const resultHandlerStrategy = new Map<string, ResultHandler>();
 
 export function initialResultHandlerStrategy() {
-  resultHandlerStrategy.set("ResultSetHeader", () => {
-      
+  let tableResultHandler: ResultHandler = (
+    result: any,
+    fields: FieldPacket[],
+    sql,
+    panel
+  ) => {
+    // assmble result
+    let messageContent = new QueryMessage(
+      Array<string>(),
+      Array<any>(),
+      sql,
+      {
+        showPaginationToolsBar: false,
+        showToolsBar: false,
+      },
+      {
+        current: 0,
+        size: 0,
+        total: 0,
+      },
+      undefined,
+      undefined
+    );
+    fields.forEach((field) => {
+      messageContent.columns.push(field.name);
+    });
+    openQueryHtml(panel);
+    panel.webview.postMessage(new Message(messageContent, true));
+  };
+
+  resultHandlerStrategy.set("ResultSetHeader", (result: ResultSetHeader) => {
+    Logger.attension(JSON.stringify(result).trim());
   });
+  resultHandlerStrategy.set("OkPacket", tableResultHandler);
 }
 
 interface ResultHandler {
-  (): void;
+  (result: any, fields: any, sql: string, panel: WebviewPanel): void;
 }
